44f87bbdde91b1236eb1a19302e97e187be54ccd
imimran <engr.aih@gmail.com>
2021-05-20 18:56:22 +0600

    TDD with Django

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,10 @@
 
+
 def add(x, y):
-    return x + y 
+    """add x, y and return result"""
+    return x + y
+
+
+def sub(x, y):
+    """Subtract x from y and return result"""
+    return (y - x)
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
-from .calc import add
+
+from .calc import add, sub
+
 
 class CalcTests(TestCase):
 
-    def test_add(self):
-        self.assertEqual(add(2,3), 5)
+    def test_add_numbers(self):
+        """Test that values are added together"""
+        self.assertEqual(add(3, 8), 11)
 
-
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(sub(5, 11), 6)
