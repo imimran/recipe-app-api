01b1dc65df2056a4ad7a24ff6fb232fc268e5d0d
imimran <engr.aih@gmail.com>
2021-05-30 11:35:53 +0600

    Create .config file

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7zs8!0*8flecibf+k65^sygsdyohxe=9)71a(w#_3s7wkifeb^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'recipeapp',
-        "USER": "postgres",
-        "PASSWORD": "123456",
-        "HOST": "localhost"
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        "USER": config('USER'),
+        "PASSWORD":config('PASSWORD'),
+        "HOST":config('HOST'),
     }
 }
 
--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import models
+from core import models
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
